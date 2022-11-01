4a179d2a9097a32d2fc1f187c7ff4fd291d56a3e
Giovanni Benner <giovanni.guilioti@benner.com.br>
2022-11-01 09:49:20 -0300

    aula03

---
+++ aula03/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Seu nome é "+ nome + " e sua idade é de "+idade+" anos.");
