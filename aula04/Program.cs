b8f51b80bfe9387271516a9f5316f2522066bde3
Giovanni Benner <giovanni.guilioti@benner.com.br>
2022-11-01 09:53:37 -0300

    exercicio aula04

---
+++ aula04/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Qual foi sua média? (0-10)");
+int media = int.Parse(Console.ReadLine());
+
+if(media >= 6)
+    Console.WriteLine("Aprovado");
+else if(media >= 4)
+    Console.WriteLine("Recuperação");
+else
+    Console.WriteLine("Reprovado");
